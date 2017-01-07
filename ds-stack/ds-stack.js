f7faad5ee015183a94d71063852a09b1a899f3bd
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-01-07 10:23:48 +0200

    [1] Stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,20 @@
 var Stack = function() {
-	//your code is here
+	//create a storage for elements 
+	this._storage=[];
+};
+
+// get a value
+Stack.prototype.add = function(value) {
+	if(this._storage.length===0){
+		return "Stroage is empty";
+	}
+	//add the element in the list 
+	// before that check if it's empty or not 
+	this._storage.push(value);
+};
+
+
+Stack.prototype.remove = function() {
+	// remove the last element in the list
+	return this._storage.pop();
 };
