0327fa45712ebf452f77d67b057e22f7f477075a
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-01-07 09:24:28 +0200

    [1] Hash Tables

--- ds-hash-table/ds-hash-table.js
+++ ds-hash-table/ds-hash-table.js
@@ -5,14 +5,20 @@ var makeHashTable = function() {
   return {
     _storage: [],
     retrieve: function(key) {
-      //your code is here
+      // create a hash value 
+      var place=hashFn(key , max);
+      // call the value by its hashed value
+      return _storage[place];
     },
 
     insert: function(key, value) {
-      //your code is here
-  };
-};
-
+      // create a hash value 
+      var place=hashFn(key, max);
+      // place the needed in value in the hashed value  
+      _storage[place]=value;
+   }
+ };
+}
 // This is a "hashing function". You don't need to worry about it, just use it to turn any key into a pseudo-random key
 var hashFn = function(str, max) {
   var hash = 0;
